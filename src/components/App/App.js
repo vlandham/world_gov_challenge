00fe0ce9a72d56b7db81b590392cf8edca186a6a
Jim Vallandingham <jvallandingham@zymergen.com>
2018-12-29 14:51:09 -0800

    Add styling

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Container } from "reactstrap";
+import { Container, Row, Col } from "reactstrap";
 
 import "./App.scss";
 
@@ -7,19 +7,24 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Container>
+          <Row>
+            <Col>
+              <p className="small-blue-title">Visualization</p>
+              <h1>Good Government</h1>
+            </Col>
+          </Row>
+          <Row>
+            <Col sm={2} />
+            <Col sm={9}>
+              <div className="line" />
+              <p>
+                What makes a good government? Some would say this is a dumb
+                question.
+              </p>
+            </Col>
+          </Row>
+        </Container>
       </div>
     );
   }

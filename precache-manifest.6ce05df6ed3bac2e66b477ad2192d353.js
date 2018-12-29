4629c8e68e1b4210158a1d532cbc4d60d55c2049
Jim Vallandingham <jvallandingham@zymergen.com>
2018-12-29 14:25:19 -0800

    Updates

---
+++ precache-manifest.6ce05df6ed3bac2e66b477ad2192d353.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "79cc8ce296d27b18d66d",
+    "url": "/world_gov_challenge/static/js/runtime~main.79cc8ce2.js"
+  },
+  {
+    "revision": "b79fd8ffcd5fe105b60e",
+    "url": "/world_gov_challenge/static/js/main.b79fd8ff.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/world_gov_challenge/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "b79fd8ffcd5fe105b60e",
+    "url": "/world_gov_challenge/static/css/main.f5a2fca2.chunk.css"
+  },
+  {
+    "revision": "544d20d15d6b685efa52acbbfc17fecd",
+    "url": "/world_gov_challenge/index.html"
+  }
+];

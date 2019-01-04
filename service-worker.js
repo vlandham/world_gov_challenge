689299cc0b8e27368d29afa09d084f622d855aa3
Jim Vallandingham <jvallandingham@zymergen.com>
2019-01-03 22:04:36 -0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/world_gov_challenge/precache-manifest.c0b52d5e82adde878c7a02378371106e.js"
+  "/world_gov_challenge/precache-manifest.5923b20b7b7ccfa89201fed44990b922.js"
 );
 
 workbox.clientsClaim();

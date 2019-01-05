3858393216e6e2e3683b0aa95bd4f23317a875a4
Jim Vallandingham <jvallandingham@zymergen.com>
2019-01-04 16:11:58 -0800

    Updates

--- precache-manifest.720e99efa1b7d2a7f0774cbc8f5616e3.js
+++ precache-manifest.720e99efa1b7d2a7f0774cbc8f5616e3.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/world_gov_challenge/static/js/runtime~main.79cc8ce2.js"
   },
   {
-    "revision": "0633c3ed1f21080fdb10",
-    "url": "/world_gov_challenge/static/js/main.0633c3ed.chunk.js"
+    "revision": "c02a664bdfad454dd561",
+    "url": "/world_gov_challenge/static/js/main.c02a664b.chunk.js"
   },
   {
     "revision": "34aff46b1db129a68b07",
     "url": "/world_gov_challenge/static/js/1.34aff46b.chunk.js"
   },
   {
-    "revision": "0633c3ed1f21080fdb10",
+    "revision": "c02a664bdfad454dd561",
     "url": "/world_gov_challenge/static/css/main.deb75baf.chunk.css"
   },
   {
-    "revision": "ba34e1c547e254b2eacb0472ba0ef382",
+    "revision": "cb6a5cd516cf157ea9a72eeaa728af87",
     "url": "/world_gov_challenge/index.html"
   }
 ];

939bc2e1e55d8bec50b612d810e8a426d046916d
Jim Vallandingham <jvallandingham@zymergen.com>
2019-01-05 06:54:25 -0800

    google analytics .

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./custom.scss";
-import "./index.scss";
-import App from "./components/App/App";
-import * as serviceWorker from "./serviceWorker";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './custom.scss';
+import './index.scss';
+import App from './components/App/App';
+import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import ReactGA from 'react-ga';
+ReactGA.initialize('UA-17355070-1');
+ReactGA.pageview(window.location.pathname + window.location.search);
+
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.

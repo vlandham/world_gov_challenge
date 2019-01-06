ae8ff284bed0d18da94c62dedc02f5104763b7e2
Jim Vallandingham <jvallandingham@zymergen.com>
2019-01-06 06:24:32 -0800

    Updates

--- precache-manifest.f685d62e9fe629095ed14f3e2bc22fe4.js
+++ precache-manifest.f685d62e9fe629095ed14f3e2bc22fe4.js
@@ -4,23 +4,23 @@ self.__precacheManifest = [
     "url": "/world_gov_challenge/static/js/runtime~main.79cc8ce2.js"
   },
   {
-    "revision": "fffb490c8294ae5aa8cc",
-    "url": "/world_gov_challenge/static/js/main.fffb490c.chunk.js"
+    "revision": "e15c7ed09d4b1e568236",
+    "url": "/world_gov_challenge/static/js/main.e15c7ed0.chunk.js"
   },
   {
-    "revision": "95a11cbe1f8702f0a4f1",
-    "url": "/world_gov_challenge/static/js/1.95a11cbe.chunk.js"
+    "revision": "9c902c51fef95915aa19",
+    "url": "/world_gov_challenge/static/js/1.9c902c51.chunk.js"
   },
   {
-    "revision": "fffb490c8294ae5aa8cc",
+    "revision": "e15c7ed09d4b1e568236",
     "url": "/world_gov_challenge/static/css/main.bc1b8347.chunk.css"
   },
   {
-    "revision": "95a11cbe1f8702f0a4f1",
+    "revision": "9c902c51fef95915aa19",
     "url": "/world_gov_challenge/static/css/1.9070bf1e.chunk.css"
   },
   {
-    "revision": "7000fcb193654d1b96cd60396542f860",
+    "revision": "d92f9e851fb81b493124a7a73c652725",
     "url": "/world_gov_challenge/index.html"
   }
 ];
